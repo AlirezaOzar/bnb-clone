45fe7a41856c09d8fb990b2e67d4f455f36fbbf7
Alireza <mohanadozarr@gmail.com>
2023-06-20 19:45:26 +0330

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ClientOnly from "./components/ClientOnly";
