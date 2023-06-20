edc4c1215edd8f702e3f339007512f02c4fe8726
Alireza <mohanadozarr@gmail.com>
2023-06-20 18:55:26 +0330

    bug 2

--- app/actions/getListing.ts
+++ app/actions/getListing.ts
@@ -11,44 +11,47 @@ export interface IListingsParams {
   category?: string;
 }
 
-export default async function getListings(parmas: IListingsParams) {
+export default async function getListings(
+  params: IListingsParams
+) {
   try {
     const {
       userId,
-      guestCount,
-      roomCount,
-      bathroomCount,
+      roomCount, 
+      guestCount, 
+      bathroomCount, 
+      locationValue,
       startDate,
       endDate,
-      locationValue,
       category,
-    } = parmas;
+    } = params;
 
     let query: any = {};
 
     if (userId) {
       query.userId = userId;
     }
+
     if (category) {
       query.category = category;
     }
 
     if (roomCount) {
       query.roomCount = {
-        gte: +roomCount,
-      };
+        gte: +roomCount
+      }
     }
 
     if (guestCount) {
       query.guestCount = {
-        gte: +guestCount,
-      };
+        gte: +guestCount
+      }
     }
 
     if (bathroomCount) {
       query.bathroomCount = {
-        gte: +bathroomCount,
-      };
+        gte: +bathroomCount
+      }
     }
 
     if (locationValue) {
@@ -62,27 +65,28 @@ export default async function getListings(parmas: IListingsParams) {
             OR: [
               {
                 endDate: { gte: startDate },
-                startDate: { lte: startDate },
+                startDate: { lte: startDate }
               },
               {
                 startDate: { lte: endDate },
-                endDate: { gte: endDate },
-              },
-            ],
-          },
-        },
-      };
+                endDate: { gte: endDate }
+              }
+            ]
+          }
+        }
+      }
     }
+
     const listings = await prisma.listing.findMany({
       where: query,
       orderBy: {
-        createdAt: "desc",
-      },
+        createdAt: 'desc'
+      }
     });
 
-    const safeListings = listings.map((lisitng) => ({
-      ...lisitng,
-      createdAt: lisitng.createdAt.toISOString(),
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
     }));
 
     return safeListings;
